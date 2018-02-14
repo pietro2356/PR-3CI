91c7331a1b6dc82c59e1fc7aba7b3da1a0c9cab7
Pietro Rocchio <pietro.rocchio@marconirovereto.it>
2018-02-14 09:32:50 +0100

    Messa a punto di JS.

--- JS/copyright.js
+++ JS/copyright.js
@@ -1,14 +1,4 @@
 function GetCopyright() {
     document.write('<h4> &copy; Copyright Rocchio Pietro 2018/2019 </h4>');
 }
-
-function GetTime() {
-    var data = new Date();
-    var ora = data.getHours();
-    var giorno = data.getDay();
-    var mese = data.getMonth();
-
-    document.write('Oggi è il ' + giorno + ' del mese ' + mese + ' e sono le ore ' + ora);
-}
 GetCopyright();
-GetTime();
--- JS/registration.js
+++ JS/registration.js
@@ -1,9 +1,7 @@
-function GetTime() {
-    var data = new Date();
-    var ora = data.getHours();
-    var giorno = data.getDay();
-    var mese = data.getMonth();
+var data = new Date();
+var ora = data.getDay;
+var giorno = data.getDay();
+var mese = data.getMonth();
 
-    document.write('Oggi è il ' + giorno + ', del mese ' + mese + ' e sono le ore ' + ora);
-}
-GetTime();
+document.write('Oggi è il ' + giorno + ', del mese ' + mese + ' e sono le ore ' + ora);
+
